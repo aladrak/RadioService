e5533c3c9216ee1afd87947f305213962dbce6e6
aladrak <sashakhong@gmail.com>
2025-12-12 00:37:26 +0700

    shell init

---
+++ AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Radiotech.Views;
+using Radiotech.Views.ListViews;
+
+namespace Radiotech;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+        
+        Routing.RegisterRoute("PersonListView", typeof(PersonListView));
+        Routing.RegisterRoute("InputView", typeof(InputView));
+    }
+}
