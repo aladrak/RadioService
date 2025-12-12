112add95dbb02a19bc90f05edbb4f5c5ae0e520b
aladrak <sashakhong@gmail.com>
2025-12-13 03:16:44 +0700

    some editing, new comments and include logic

--- Data/TableData.cs
+++ Data/TableData.cs
@@ -4,7 +4,7 @@
 	{
 		public class Person 
 		{
-			public int PersonID { get; set; }
+			public int PersonID { get; init; }
 			public string FirstName { get; set; }
 			public string MidName { get; set; }
 			public string LastName { get; set; }
@@ -19,33 +19,33 @@
 
 		public class Company
 		{
-			public int CompanyID { get; set; }
-			public int PersonID { get; set; }
+			public int CompanyID { get; init; }
+			public int? PersonID { get; set; }
 			public string Bank { get; set; }
-			public string AccountNum { get; set; }
+			public string AccountNum { get; set; } // 20 digits
 			public string ITN { get; set; } // ИНН для организаций 10 для физ лиц 12
 		}
 
 		public class Product
 		{
-			public int ProductID { get; set; }
+			public int ProductID { get; init; }
 			public string Mark { get; set; }
 			public string Country { get; set; }
 			public string Manufacturer { get; set; }
-			public string Photo { get; set; }
+			public string Photo { get; set; } // link to photo
 			public DateTime ServiceLife { get; set; }
 		}
 
 		public class Specialty
 		{
-			public int SpecialtyID { get; set; }
+			public int SpecialtyID { get; init; }
 			public string Name { get; set; }
 			public string Description { get; set; }
 		}
 
 		public class Employee
 		{
-			public int EmployeeID { get; set; }
+			public int EmployeeID { get; init; }
 			public int SpecialtyID { get; set; }
 			public string FirstName { get; set; }
 			public string MidName { get; set; }
@@ -58,13 +58,13 @@
 
 		public class Order
 		{
-			public int OrderID { get; set; }
+			public int OrderID { get; init; }
 			public int ProductID { get; set; }
 			public int CompanyID { get; set; }
 			public int EmployeeID { get; set; }
-			public DateTime StartDate { get; set; }
+			public DateTime StartDate { get; set; } // DateTime field
 			public DateTime FinishDate { get; set; }
-			public List<string> FaultsList { get; set; }
+			public List<string> FaultsList { get; set; } // thinking...
 			public double Price { get; set; }
 		}
 	}
