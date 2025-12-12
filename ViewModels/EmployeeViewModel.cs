90b632ac6d3f4f35e240b499dea7215e310e6be0
aladrak <sashakhong@gmail.com>
2025-12-13 03:14:51 +0700

    init EmployeeList

---
+++ ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using Radiotech.Data;
+
+namespace Radiotech.ViewModels;
+
+public class EmployeeViewModel
+{
+    public int FreeId => Employees.Max(p => p.EmployeeID) + 1;
+    public ObservableCollection<TableData.Employee> Employees
+    {
+        get => _employees;
+        private set => _employees = value; 
+    }
+    private ObservableCollection<TableData.Employee> _employees = [];
+    private readonly ITableRepository<TableData.Employee> _repository;
+
+    public EmployeeViewModel()
+    {
+        _repository = new TableRepository<TableData.Employee>("employee.json");
+        Employees = new ObservableCollection<TableData.Employee>(_repository.GetAll());
+    }
+    public void Add(TableData.Employee e)
+    {
+        Employees.Add(e);
+        _repository.InsertOrUpdate(Employees.ToList());
+    }
+    public void Delete(TableData.Employee e) // TODO: Checking dependencies
+    {
+        Employees.Remove(e);
+        _repository.InsertOrUpdate(Employees.ToList());
+    }
+    public void Update(TableData.Employee e)
+    {
+        Employees.Insert(Employees.IndexOf(e), e);
+        _repository.InsertOrUpdate(Employees.ToList());
+    }
+}
--- Views/ListViews/EmployeeListView.cs
+++ Views/ListViews/EmployeeListView.cs
@@ -1,6 +1,161 @@
+using Radiotech.Common;
+using Radiotech.Data;
+using Radiotech.ViewModels;
+
 namespace Radiotech.Views.ListViews;
 
 public class EmployeeListView : ContentPage
 {
-    
+    private readonly CollectionView _collectionView;
+    private readonly EmployeeViewModel _viewModel;
+
+    public EmployeeListView()
+    {
+        _viewModel = new EmployeeViewModel();
+
+        _collectionView = new CollectionView
+        {
+            SelectionMode = SelectionMode.Single,
+            ItemsSource = _viewModel.Employees,
+            ItemTemplate = CollectionItem()
+        };
+        _collectionView.SelectionChanged += async (_, e) =>
+        {
+	        if (e.CurrentSelection.FirstOrDefault() is not TableData.Employee selectedItem)
+	        {
+		        _collectionView.SelectedItem = null;
+		        return;
+	        }
+	        
+	        _collectionView.SelectedItem = null;
+	        
+	        string action = await Shell.Current.DisplayActionSheetAsync(
+		        $"Action on «{selectedItem}»",
+		        "Cancel",
+		        null,
+		        "Edit",
+		        "Delete"
+	        );
+
+	        if (action == "Edit")
+	        {
+		        // TODO: Edit
+	        }
+	        else if (action == "Delete")
+	        {
+		        bool confirm = await Shell.Current.DisplayAlertAsync(
+			        "Confirmation",
+			        $"Delete «{selectedItem}»?",
+			        "Yes", "No"
+		        );
+
+		        if (confirm)
+		        {
+			        _viewModel.Delete(selectedItem);
+		        }
+	        }
+        };
+        var addButton = new Button{ Text = "Add Employee" };
+		addButton.Clicked += ShowInputView;
+		Content = new ScrollView
+		{
+			Content = new VerticalStackLayout
+			{
+				Spacing = 24,
+				Padding = 4,
+				Children = { _collectionView, addButton }
+			}
+		};
+    }
+
+    private static DataTemplate CollectionItem()
+    {
+	    return new DataTemplate(() =>
+	    {
+		    var grid = new Grid
+		    {
+			    VerticalOptions = LayoutOptions.Center,
+			    Padding = 10,
+			    ColumnSpacing = 10,
+			    ColumnDefinitions =
+			    {
+				    new ColumnDefinition { Width = 40 },
+				    new ColumnDefinition { Width = GridLength.Star },
+				    new ColumnDefinition { Width = GridLength.Star },
+				    new ColumnDefinition { Width = GridLength.Star },
+				    new ColumnDefinition { Width = GridLength.Star },
+				    new ColumnDefinition { Width = GridLength.Star },
+				    new ColumnDefinition { Width = GridLength.Star },
+				    new ColumnDefinition { Width = GridLength.Star }
+			    }
+		    };
+
+		    var idLabel = new Label();
+		    idLabel.SetBinding(Label.TextProperty, nameof(TableData.Employee.EmployeeID));
+		    grid.Add(idLabel, 0, 0);
+
+		    var lastNameLabel = new Label();
+		    lastNameLabel.SetBinding(Label.TextProperty, nameof(TableData.Employee.LastName));
+		    grid.Add(lastNameLabel, 1, 0);
+
+		    var firstNameLabel = new Label();
+		    firstNameLabel.SetBinding(Label.TextProperty, nameof(TableData.Employee.FirstName));
+		    grid.Add(firstNameLabel, 2, 0);
+
+		    var midNameLabel = new Label();
+		    midNameLabel.SetBinding(Label.TextProperty, nameof(TableData.Employee.MidName));
+		    grid.Add(midNameLabel, 3, 0);
+
+		    var addressLabel = new Label();
+		    addressLabel.SetBinding(Label.TextProperty, nameof(TableData.Employee.Address));
+		    grid.Add(addressLabel, 4, 0);
+
+		    var phoneLabel = new Label();
+		    phoneLabel.SetBinding(Label.TextProperty, nameof(TableData.Employee.Phone));
+		    grid.Add(phoneLabel, 5, 0);
+		    
+		    var ageLabel = new Label();
+		    ageLabel.SetBinding(Label.TextProperty, nameof(TableData.Employee.Age));
+		    grid.Add(ageLabel, 6, 0);
+		    
+		    var skillLabel = new Label();
+		    phoneLabel.SetBinding(Label.TextProperty, nameof(TableData.Employee.Skill));
+		    grid.Add(skillLabel, 7, 0);
+
+		    return grid;
+	    });
+    }
+    private async void ShowInputView(object? sender, EventArgs e)
+    {
+	    List<(string, string[]?, DelegateValidator)> configFields = [
+		    ("Специальность", ["Рыбак", "Кучер"], Validators.RequiredNotNull),
+		    ("Имя", null, Validators.RequiredLettersOnly),
+		    ("Отчество", null, Validators.RequiredMidName),
+		    ("Фамилия", null, Validators.RequiredLettersOnly),
+		    ("Адрес", null, Validators.RequiredNotNull),
+		    ("Телефон", null, s => Validators.RequiredDigitsOnlyFixedLength(s, 11))
+	    ];
+	    var inputView = new InputView<TableData.Employee>(
+		    "New Employee", 
+		    configFields,
+		    (fields, _) => new TableData.Employee
+		    {
+			    EmployeeID = _viewModel.FreeId,
+			    SpecialtyID = -1,
+			    FirstName = fields[0].CurrentValue ?? "",
+			    MidName = fields[1].CurrentValue ?? "",
+			    LastName = fields[2].CurrentValue ?? "",
+			    Address = fields[3].CurrentValue ?? "",
+			    Phone = fields[4].CurrentValue ?? "",
+			    Age = -1,
+			    Skill = -1
+		    },
+		    onSuccess: result =>
+		    {
+			    _viewModel.Add(result);
+			    DisplayAlertAsync("Success", $"You entered: {result}", "OK");
+		    }
+	    );
+	    await Navigation.PushModalAsync(inputView);
+    }
 }
