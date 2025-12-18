7d722f7e06f4eb2a404ccbc0aaf8df4b169fb7f2
aladrak <sashakhong@gmail.com>
2025-12-19 02:37:58 +0700

    interface IInputControl added

--- Common/ValidationTypes.cs
+++ Common/ValidationTypes.cs
@@ -1,3 +1,14 @@
 ﻿namespace Radiotech.Common;
 
 public delegate (bool isValid, string error) DelegateValidator(string? field);
+
+public interface IInputControl
+{
+    View Control { get; }
+    Label ErrorLabel { get; }
+    bool IsValid { get; }
+    object? GetValue();
+    // void Validate();
+    event Action<string?>? ValueChanged;
+    void SetValue(object? value);
+}
