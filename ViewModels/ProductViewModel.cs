789452d92253834decbac1e4ee8497c27f55ae28
aladrak <sashakhong@gmail.com>
2025-12-19 03:07:19 +0700

    inition

---
+++ ViewModels/ProductViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using Radiotech.Data;
+
+namespace Radiotech.ViewModels;
+
+public class ProductViewModel
+{
+    public int FreeId => Products.Count() != 0 ? Products.Max(p => p.ProductID) + 1 : 1;
+    public ObservableCollection<TableData.Product> Products
+    {
+        get => _products; 
+        private set => _products = value; 
+    }
+    private ObservableCollection<TableData.Product> _products = [];
+    private readonly ITableRepository<TableData.Product> _repository;
+
+    public ProductViewModel()
+    {
+        _repository = new TableRepository<TableData.Product>("product.json");
+        Products = new ObservableCollection<TableData.Product>(_repository.GetAll());
+    }
+    public void Add(TableData.Product e)
+    {
+        Products.Add(e);
+        _repository.InsertOrUpdate(Products.ToList());
+    }
+    public void Delete(TableData.Product e) // TODO: Checking dependencies
+    {
+        Products.Remove(e);
+        _repository.InsertOrUpdate(Products.ToList());
+    }
+    public void Update(TableData.Product old, TableData.Product cur)
+    {
+        Products[Products.IndexOf(old)] = cur;
+        _repository.InsertOrUpdate(Products.ToList());
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+using Radiotech.Data;
+
+namespace Radiotech.ViewModels;
+
+public class ViewModelBase<T>
+{
+    // public int FreeId => DataList.Count != 0 ? DataList.Max(p => p.) + 1 : 1;
+    // public ObservableCollection<T> DataList
+    // {
+    //     get => _data;
+    //     private set => _data = value; 
+    // }
+    // private ObservableCollection<T> _data = [];
+}
